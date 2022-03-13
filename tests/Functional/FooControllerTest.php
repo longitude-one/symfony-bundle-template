ddbe52962ae03ec2f587995050912f02f2d3c705
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2022-03-13 14:41:19 +0100

    Fixing a test

--- tests/Functional/FooControllerTest.php
+++ tests/Functional/FooControllerTest.php
@@ -3,24 +3,19 @@
 namespace LongitudeOne\CookiesConsentBundle\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class FooControllerTest extends WebTestCase
 {
     private ?KernelBrowser $client = null;
 
-    private ?ContainerInterface $container = null;
-
     public function setUp(): void
     {
         $this->client = static::createClient();
-        $this->container = $this->client->getContainer();
     }
 
     public function tearDown(): void
     {
         $this->client = null;
-        $this->container = null;
     }
 
     public function testBarAction()
